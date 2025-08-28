9edd561c25912d59d8012dff88bce6a177b448de
endersonch <enderson.desing@gmail.com>
2025-08-28 18:48:51 -0400

    Creacion de funciones principales

--- app.js
+++ app.js
@@ -1,1 +1,46 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+let numeroAleatoreo = generarNumeroAleatoreo();
+
+function agregarAmigo(){
+
+    let amigo = document.getElementById("amigo").value;
+
+    if (amigo == ""){
+        alert("Ups! Entrada vacia, ingrese una nombre");
+    }else{
+    amigos.push(document.getElementById("amigo").value);
+    listarAmigos();
+    }
+
+}
+
+function listarAmigos(){
+    const listaDeAmigos = document.getElementById('listaAmigos');
+    const nuevaLinea =document.createElement("li");
+    nuevaLinea.textContent = amigos[amigos.length-1];
+    listaDeAmigos.append(nuevaLinea);      
+    }
+
+function sortearAmigo(){
+
+    if(amigos.length>0){
+      const i=parseInt(generarNumeroAleatoreo())  
+      console.log(i);
+      const resultado = document.getElementById("resultado");
+      const linea= document.createElement("li");
+      linea.textContent=amigos[i];
+      resultado.clear();
+      resultado.append(linea);
+      console.log(linea)
+    }else{
+        alert("No Hay amigos aun en la lista")
+    }
+    
+
+
+}
+
+function generarNumeroAleatoreo(){
+    return Math.floor(Math.random()*(amigos.length));
+}
